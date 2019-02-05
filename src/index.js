8d037838eee00ea93c2e3595be5ef9b37b086865
Ilya Geller <ilya@chatway.com>
2019-02-05 18:09:20 +0200

    new repo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class ActionButton extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            num:0,
+            show: true
+        }
+    }
+    tick(){
+        const newnum = this.state.num + 1;
+        this.setState({ num: newnum });
+    }
+    
+    componentDidMount(){
+        this.timerID = setInterval(
+            ()=>this.tick(),
+            1000
+        );
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+    }
+
+    handleClick(){
+        this.setState({ show: false });
+        this.componentWillUnmount();
+    }
+
+    render() { 
+        return ( 
+            this.state.show ? (<button onClick={()=>this.handleClick()}>{this.state.num}</button>) : null
+         );
+    }
+}
+ 
+const element = <ActionButton/>;
+ReactDOM.render(element, document.getElementById("root"));
